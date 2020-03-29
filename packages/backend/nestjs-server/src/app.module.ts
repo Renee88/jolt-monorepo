fd4d44733eb8868961e70b46a4829f4798577aba
Renee Cohen <reneecohen88@gmail.com>
2020-03-29 18:24:01 +0300

    rooms api - complete

--- packages/backend/nestjs-server/src/app.module.ts
+++ packages/backend/nestjs-server/src/app.module.ts
@@ -3,11 +3,11 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {UsersModule} from "./users/users.module";
 import {TalksModule} from "./talks/talks.module";
+import {RoomsModule} from "./rooms/rooms.module";
 
 @Module({
-    imports: [UsersModule, TalksModule],
+    imports: [UsersModule, TalksModule, RoomsModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ packages/backend/nestjs-server/src/rooms/rooms.controller.ts
@@ -0,0 +1,52 @@
+import {Body, Controller, Delete, Get, Param, Post} from "@nestjs/common";
+import {RoomsService} from "./rooms.service";
+import {APIResponse, Data} from "../responsesTypes";
+
+@Controller('rooms')
+export class RoomsController {
+    constructor(private readonly RoomsService: RoomsService) {
+    }
+
+    @Get()
+    getRooms(): APIResponse<Data[]> {
+        return {
+            data: this.RoomsService.getRooms(),
+            success: true
+        }
+    }
+
+    @Get(':id')
+    getRoom(
+        @Param('id') roomId: string
+    ): APIResponse<Data> {
+        return {
+            data: this.RoomsService.getRoom(roomId),
+            success: true
+        }
+    }
+
+    @Post()
+    addRoom(
+        @Body('name') roomName: string,
+        @Body('talk') talk: string,
+        @Body('jolter') jolter: string
+    ): APIResponse<Data> {
+        const id = this.RoomsService.addRoom(roomName, talk, jolter);
+        return {
+            data: {id},
+            success: true
+        }
+    }
+
+    @Delete(':id')
+    deleteRoom(
+        @Param('id') roomId: string
+    ): APIResponse<string> {
+        this.RoomsService.deleteRoom(roomId)
+        return {
+            data: roomId,
+            success: true
+        }
+    }
+
+}
