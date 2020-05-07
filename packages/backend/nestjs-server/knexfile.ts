a7b277edae3bc22f598d33878804bf9d931bbf13
Renee88 <reneecohen88@gmail.com>
2020-05-07 14:42:13 +0300

    mysql dockerized

--- packages/backend/nestjs-server/knexfile.ts
+++ packages/backend/nestjs-server/knexfile.ts
@@ -1,45 +1,47 @@
 require('dotenv').config();
 module.exports = {
-
   development: {
     client: 'mysql',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.USER,
-      password : process.env.PW,
-      database : 'jolt_training'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: 'mydb',
+      insecureAuth: true,
     },
     migrations: {
       directory: './data/migrations',
     },
-    seeds: { directory: './data/seeds' }
+    seeds: { directory: './data/seeds' },
   },
 
   testing: {
     client: 'mysql',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.USER,
-      password : process.env.PW,
-      database : 'jolt_training'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: 'mydb',
+      insecureAuth: true,
     },
     migrations: {
       directory: './data/migrations',
     },
-    seeds: { directory: './data/seeds' }
+    seeds: { directory: './data/seeds' },
   },
 
   production: {
     client: 'mysql',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.USER,
-      password : process.env.PW,
-      database : 'jolt_training'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: 'mydb',
+      insecureAuth: true,
     },
     migrations: {
       directory: './data/migrations',
     },
-    seeds: { directory: './data/seeds' }
-  }
+    seeds: { directory: './data/seeds' },
+  },
 };
